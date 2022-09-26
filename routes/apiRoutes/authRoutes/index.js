8bd9370a38b0a3c87d291473c12003e231d85490
Adrian Romero <radrian031613@gmail.com>
2022-09-26 08:46:07 -0700

    set up router for log-in

--- routes/apiRoutes/authRoutes/index.js
+++ routes/apiRoutes/authRoutes/index.js
@@ -12,6 +12,19 @@ const {
  */
 
  // dynamc router for register new users
+ // can add function to check the role of user before allowing registration
+ // in the case type where client wants users with certain roles to create user registration
  router.post('/register/:role', userAuth, async (req, res) => {
    await userRegister(req.body, res);
  })
+
+ /**
+  * @DESC router for logging in 
+  */
+
+  // users */api/auth
+  router.post('/login/:role', async (req, res) => {
+    await userLogin(req.body, res);
+  })
+
+module.exports = router;
